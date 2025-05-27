027802c0d0b59845dc72bc1ce2aed03b59f5ff23
afzaalis <afzaalisnaufal03@gmail.com>
2025-05-27 10:50:12 +0700

    update env

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
+require('dotenv').config(); 
 
 const db = mysql.createPool({
-  host: 'shinkansen.proxy.rlwy.net',
-  user: 'root',
-  password: 'JNkffZDguCtpxRHQYenkKzZfczxfmDKI',
-  database: 'railway',
-  port: 55011,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 const promiseDb = db.promise();
